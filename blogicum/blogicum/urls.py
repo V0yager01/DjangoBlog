51461542f10b2a3818b477b39a586bbf76c15350
ilyas <alexsinotov4710@gmail.com>
2023-07-16 00:39:18 +0500

    v1.1112

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -15,11 +15,8 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path(
         'auth/registration/',
-        CreateView.as_view(
-             template_name='registration/registration_form.html',
-             form_class=UserCreationForm,
-             success_url=reverse_lazy('blog:index'),
-             ),
-        name='registration',
-        ),
+        CreateView.as_view(template_name='registration/registration_form.html',
+                           form_class=UserCreationForm,
+                           success_url=reverse_lazy('blog:index'),),
+        name='registration',),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
